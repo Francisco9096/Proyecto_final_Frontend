303551dcb88563e988c48cfa8b72172f0532f6af
Francisco Murillo <fmurillo9096@universidadean.edu.co>
2026-05-26 08:47:53 -0500

    init: backend Django con modelos, admin y endpoint base

---
+++ backen/core/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Ingrediente(models.Model):
+    nombre = models.CharField(max_length=100)
+    precio = models.FloatField()
+    calorias = models.IntegerField()
+    inventario = models.IntegerField()
+    tipo = models.CharField(max_length=20)  # base o complemento
+
+    def __str__(self):
+        return self.nombre
+
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    precio_publico = models.FloatField()
+    ingredientes = models.ManyToManyField(Ingrediente)
+
+    def __str__(self):
+        return self.nombre

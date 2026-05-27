d4e5ddac42b84b58ca7224368aaff7fcbf61bc5c
Francisco Murillo <fmurillo9096@universidadean.edu.co>
2026-05-27 09:35:20 -0500

    Adicion de supabase funcional, enlazando el frontend y el backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,38 +5,75 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 */ 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { supabase } from "./supabaseClient";
+import "./App.css";
 
 function App() {
+  const [productos, setProductos] = useState([]);
   const [mensaje, setMensaje] = useState("");
 
-  const venderProducto = async () => {
+  useEffect(() => {
+    const fetchProductos = async () => {
+      const { data, error } = await supabase
+        .from("productos")
+        .select("*");
+
+      if (error) {
+        console.error("Error Supabase:", error);
+      } else {
+        console.log("Productos:", data);
+        setProductos(data);
+      }
+    };
+
+    fetchProductos();
+  }, []);
+
+  const venderProducto = async (id) => {
     try {
-      const response = await fetch("http://127.0.0.1:8000/api/vender/1/", {
+      const response = await fetch(`http://127.0.0.1:8000/api/vender/${id}/`, {
         method: "POST",
       });
 
       const data = await response.json();
       setMensaje(data.mensaje || data.error);
-    } catch (error) {
-      console.error(error);
-      setMensaje("Error conectando con el backend");
+    } catch {
+      setMensaje("Error conectando con backend");
     }
   };
 
   return (
-    <div style={{ padding: "40px" }}>
-      <h1>Heladería 🍦</h1>
+  <div className="container">
+    <h1 className="title">Heladería 🍦</h1>
 
-      <button onClick={venderProducto}>
-        Vender Copa Vainilla
-      </button>
+    <div className="product-list">
+      {productos.map((producto) => (
+        <div className="card" key={producto.id}>
+          <h3>{producto.nombre}</h3>
+          <p className="price">Precio: ${producto.precio_publico}</p>
 
-      <p>{mensaje}</p>
+          <button onClick={() => venderProducto(producto.id)}>
+            Vender
+          </button>
+        </div>
+      ))}
     </div>
+
+    
+    {mensaje && (
+      <p style={{
+        marginTop: "20px",
+        color: "#22c55e",
+        fontSize: "1.2rem",
+        fontWeight: "bold"
+      }}>
+        ✅ {mensaje}
+      </p>
+    )}
+
+  </div>
   );
 }
 
 export default App;
-
-
--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://TU_URL.supabase.co';
-const supabaseKey = 'TU_ANON_KEY';
+const supabaseUrl = 'https://pekfawtqljqfhtfoxfqc.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBla2Zhd3RxbGpxZmh0Zm94ZnFjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4NDc0ODUsImV4cCI6MjA5NTQyMzQ4NX0.hs4BX4gG7alPAbkmJORS4Z9gR1NKbIV9nqvp9De7gy8';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 

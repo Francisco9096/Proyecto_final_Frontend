34c9c65369b32352823a533e1797ea68f312571b
Francisco Murillo <fmurillo9096@universidadean.edu.co>
2026-05-27 00:14:52 -0500

    Instalacion de subbase

---
+++ frontend/src/supabaseClient.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://TU_URL.supabase.co';
+const supabaseKey = 'TU_ANON_KEY';
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
+
